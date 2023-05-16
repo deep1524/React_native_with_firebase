5fc03a0d2cabd781870785012d1466bf09fc8f1e
Deepkamal yadav <dkits1995@gmail.com>
2023-05-16 17:03:35 +0530

    firebase get

---
+++ App.js
@@ -0,0 +1,35 @@
+import React, { useEffect, useState } from 'react';
+import {Text, View} from 'react-native';
+import firestore from '@react-native-firebase/firestore';
+
+const userDocument = firestore().collection('Users').doc('ABC'); 
+
+const App = () => {
+  const [user,setUser]=useState("");
+
+  const getdata= async()=>{
+    try {
+      let data=await firestore().collection('user').doc("KQ5eKlw5jn7S5neY6H9H").get(); 
+      setUser(data._data);
+      console.log(data._data.hobby);
+    } catch (error) {
+      console.log(error)
+    }
+
+  }
+  useEffect(()=>{
+    getdata();
+  },[]);
+  return (
+    <View>
+      <Text>Name:{user?user.name:"Loading"}</Text>
+      <Text>Name:{user?user.age:"Loading"}</Text>
+     
+      <Text>Name:{user?user.hobby.map((list)=>`  ${list}`):"Loading"}</Text>
+
+    </View>
+  );
+};
+
+export default App;
+
