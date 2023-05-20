d058e2176dbd27ad45d2587f65eed452e5263d23
Deepkamal yadav <dkits1995@gmail.com>
2023-05-20 23:29:33 +0530

    storage

--- src/Screen/ImageUpload.jsx
+++ src/Screen/ImageUpload.jsx
@@ -8,24 +8,38 @@ import storage from '@react-native-firebase/storage';
 
 const ImageUpload = () => {
   const [imagedata, setImagedata] = useState("");
+  const [fullimagerefpath, setfullimagerefpath] = useState("");
+  const [imageurl, setimageurl] = useState("");
   const pickimage = async () => {
     try {
       const response = await DocumentPicker.pickSingle({
         type: [DocumentPicker.types.images],
+        copyTo:"cachesDirectory",
       });
       console.log(response);
       setImagedata(response);
+     
     } catch (error) {
       console.log(error);
     }
   };
-  const uploadimage=async()=>{
+  const uploadimage=  async()=>{
     try {
-        const response=await storage().ref("/profile/1.png").putFile(imagedata.uri);
+        const response= await storage().ref(`/profile/${imagedata.name}`).putFile(imagedata.fileCopyUri);
        console.log(response);
+       setfullimagerefpath(response.metadata.fullPath)
+      //  const url= await response.ref.getDownloadURL();
+      //  setimageurl(url);
     } catch (error) {
         console.log(error);
         
+    }
+  }
+  const deleteimage=()=>{
+    try {
+      
+    } catch (error) {
+      console.log(error);
     }
   }
   return (
@@ -38,6 +52,10 @@ const ImageUpload = () => {
       <Button title="Select Image" onPress={() => pickimage()}></Button>
 
       <Button title="Upload Image" onPress={() => uploadimage()}></Button>
+      <Button title="Delete Image" onPress={() => deleteimage()} color="red"></Button>
+      </View>
+      <View>
+        <Text>Url:-{imageurl.length>0?imageurl:"Url not found"}</Text>
       </View>
     </View>
   );
